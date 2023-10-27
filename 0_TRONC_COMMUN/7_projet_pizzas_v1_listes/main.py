af7fbea4ac8b334c0ea80baf42f557bd4d4f81f3
jonathan.jory <jonathan.jory@etu.univ-orleans.fr>
2023-10-27 09:10:13 +0200

    projet pizza

--- 0_TRONC_COMMUN/7_projet_pizzas_v1_listes/main.py
+++ 0_TRONC_COMMUN/7_projet_pizzas_v1_listes/main.py
@@ -1,51 +1,57 @@
+"""Utilisation de listes pour gérer des collections de pizzas"""
 # def tri_personnalise(e):
 #    return len(e)
 
 
 def afficher(collection, n_premiers_elements=-1):
-    # "---- LISTE DES PIZZAS (4) ----"
-    # afficher les pizzas 1 pizza par ligne
-    # "AUCUNE PIZZA"
-    # collection.sort(reverse=True, key=tri_personnalise)
-    c = collection
-    if not n_premiers_elements == -1:
-        c = collection[:n_premiers_elements]
+    """
+    La fonction `afficher` prend une collection de pizzas et éventuellement un certain nombre de
+    premiers éléments à afficher, trie la collection et imprime la liste des pizzas avec
+    la première et la dernière pizza.
 
-    nb_pizzas = len(c)
+    :param collection: Le paramètre "collection" est une liste de noms de pizzas. Il représente la
+    collection de pizzas que nous souhaitons exposer
+    :param n_premiers_elements: Le paramètre `n_premiers_elements` est un paramètre facultatif qui
+    spécifie le nombre d'éléments de la `collection` qui doivent être affichés. Par défaut, il est
+    défini sur -1, ce qui signifie que tous les éléments de la « collection » seront affichés
+    :return: La fonction ne renvoie explicitement rien. Cependant, il imprime la liste des pizzas et
+    quelques informations complémentaires.
+    """
+    # collection.sort(key=tri_personnalise)
+    pizza_liste = collection
+    if n_premiers_elements != -1:
+        pizza_liste = collection[:n_premiers_elements]
+    nb_pizzas = len(pizza_liste)
     if nb_pizzas == 0:
         print("AUCUNE PIZZA")
         return
-
+    pizza_liste.sort()
     print(f"---- LISTE DES PIZZAS ({nb_pizzas}) ----")
-    for i in c:
+    for i in pizza_liste:
         print(i)
     print()
-    print("Première pizza: " + c[0])
-    print("Dernière pizza: " + c[-1])
-    # première pizza
-    # la dernière pizza
+    print("Première pizza: " + pizza_liste[0])
+    print("Dernière pizza: " + pizza_liste[-1])
 
 
 def ajouter_pizza_utilisateur(collection):
-    p = input("Pizza à ajouter: ")
-    #if pizza_existe(p, collection):
-    if p.lower() in collection:
+    """
+    La fonction "ajouter_pizza_utilisateur" permet à l'utilisateur d'ajouter une pizza
+    à une collection, en vérifiant si elle existe déjà avant de l'ajouter.
+
+    :param collection: Le paramètre "collection" est une liste qui contient les pizzas existantes
+    """
+    entre_pizza = input("Pizza à ajouter: ")
+    if entre_pizza.lower() in collection:
         print("ERREUR : Cette pizza existe déjà")
     else:
-        collection.append(p)
+        collection.append(entre_pizza)
 
 
-# def pizza_existe(e, collection):
-#     for i in collection:
-#         if i == e:
-#             return True
-#     return False
+
 
 pizzas = ["4 fromages", "végétarienne", "hawai", "calzone"]
 
-# pizza_existe -> bool
-#   True : la pizza existe -> print ("ERREUR : la pizza existe déjà")
-#   False : elle n'existe pas -> append
 ajouter_pizza_utilisateur(pizzas)
 
-afficher(pizzas, 6)
+afficher(pizzas, 2)
